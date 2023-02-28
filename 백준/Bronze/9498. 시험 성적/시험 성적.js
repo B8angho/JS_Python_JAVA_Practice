1fcaea4997db3e10168ec88150f6804bbf6ac88b
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-02-28 14:05:56 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
 const a = Number(input[0]);
 
-if (a >= 90) {
+if (a >= 90 && a <= 100) {
   console.log("A");
 } else if (a >= 80) {
   console.log("B");
