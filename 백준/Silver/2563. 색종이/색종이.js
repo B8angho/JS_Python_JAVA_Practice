f4ffe4a2f9c8474a488109fcc0f1c2a66e1a54d6
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-03-22 20:28:41 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 9668 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.js
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -1,18 +1,20 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const grid = Array.from(Array(100), () => new Array(100).fill(false));
+let arr = Array.from(new Array(100), () => Array(100).fill(0));
+const num = input.shift();
+let answer = 0;
 
-let area = 0;
-for (let i = 0; i < input.length; i++) {
-  const [x, y] = input[i].split(" ").map(Number);
-  for (let r = x; r < x + 10; r++) {
-    for (let c = y; c < y + 10; c++) {
-      if (!grid[r][c]) {
-        grid[r][c] = true;
-        area++;
+for (let i = 0; i < num; i++) {
+  let x = +input[i].split(" ")[0];
+  let y = +input[i].split(" ")[1];
+
+  for (let j = x; j < x + 10; j++) {
+    for (let k = y; k < y + 10; k++) {
+      if (arr[j][k] === 0) {
+        arr[j][k] = 1;
+        answer++;
       }
     }
   }
 }
-
-console.log(area);
+console.log(answer);
