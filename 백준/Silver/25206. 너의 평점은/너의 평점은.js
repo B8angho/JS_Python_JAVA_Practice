8cde68ed2ab137ff7b514c5ef4cde3f62ca6890c
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-03-22 16:36:20 +0900

    [Silver V] Title: 너의 평점은, Time: 124 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -0,0 +1,27 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => x.split(" "));
+
+const table = {
+  "A+": 4.5,
+  A0: 4.0,
+  "B+": 3.5,
+  B0: 3.0,
+  "C+": 2.5,
+  C0: 2.0,
+  "D+": 1.5,
+  D0: 1.0,
+  F: 0.0,
+};
+
+let s = 0;
+let t = 0;
+for (let [name, credit, grade] of input) {
+  if (grade === "P") continue;
+  s += parseFloat(credit);
+  t += parseFloat(credit) * table[grade];
+}
+console.log((t / s).toFixed(6));
