2cab247416dfc8be9f0273c18fbe9e91e222551d
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-04-26 16:32:44 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 9.93 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        String a = my_string.substring(s, s + overwrite_string.length());
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        answer = before + overwrite_string + after;
+        return answer;
+    }
+}
