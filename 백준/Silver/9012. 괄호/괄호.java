277981c4c6f84c207c0032680f9163fc80501fda
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-05-09 16:29:18 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		Stack<Character> stack = new Stack<>();
+
+		for(int i = 0; i < num; i++) {
+			String str = br.readLine();
+			int length = str.length();
+
+			for(int j = 0; j < length; j++) {
+				char ch = str.charAt(j);
+
+				if (ch == '(') {
+					stack.push(ch);
+				}
+				else {
+					int size = stack.size();
+					if(size == 0) {
+						stack.push(ch);
+						break;
+					}
+					else {
+						stack.pop();
+					}
+				}
+			}
+			if(stack.isEmpty()) {
+				System.out.println("YES");
+			}
+			else {
+				System.out.println("NO");
+			}
+
+			stack.clear();
+		}
+	}
+
+}
