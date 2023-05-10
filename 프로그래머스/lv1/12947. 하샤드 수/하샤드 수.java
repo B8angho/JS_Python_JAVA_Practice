004cc02a901dd1acace51a4c534e6d8893b36d6a
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-05-10 14:58:22 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int a = x;
+        
+        while(a >= 1) {
+            sum += a % 10;
+            a /= 10;
+        }
+    return (x % sum == 0) ? true : false;
+    }
+}
