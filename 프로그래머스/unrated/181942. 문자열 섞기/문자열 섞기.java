8a22bc5b11a0d2519a00209b45f5b91f76954d9f
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-05-13 11:36:03 +0900

    [unrated] Title: 문자열 섞기, Time: 1.20 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,9 +1,10 @@
 class Solution {
     public String solution(String str1, String str2) {
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < str1.length(); i++) {
-            answer.append(str1.charAt(i)).append(str2.charAt(i));
+        String answer = "";
+        for(int i = 0; i < str1.length(); i++) {
+            answer += str1.substring(i,i+1);
+            answer += str2.substring(i,i+1);
         }
-        return answer.toString();
+        return answer;
     }
 }
