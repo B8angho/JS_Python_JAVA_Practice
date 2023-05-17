1e56aecb5643b31dbbc640f7ad79be2214026b77
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-05-17 14:08:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -15,3 +15,15 @@ function solution(n, arr1, arr2) {
     }
     return answer;
 }
+/*
+function solution(n, arr1, arr2) {
+  const answer = [];
+
+  for (let i = 0; i < n; i++) {
+    const binary = (arr1[i] | arr2[i]).toString(2).padStart(n, '0');
+    const line = [...binary].map((digit) => (digit === '1' ? '#' : ' ')).join('');
+    answer.push(line);
+  }
+
+  return answer;
+} */
