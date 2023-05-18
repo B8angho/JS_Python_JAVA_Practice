caeeba935b4aef56ccd7c060df7dbe5017212171
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-05-18 16:19:41 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -2,7 +2,7 @@ function solution(numbers, n) {
     var answer = 0;
     for(let i = 0; i < numbers.length; i++) {
         answer += numbers[i];
-        if(answer > n) return answer;
+        if(answer > n) break;
     }
-   
+   return answer;
 }
