4bfad2cfeeef1f02fb2bd8404f758ef1b336e91c
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-05-18 13:05:09 +0900

    [Bronze II] Title: 벌집, Time: 160 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let range = 1,
+  block = 1;
+
+while (block < input) {
+  block += 6 * range;
+  range++;
+}
+console.log(range);
