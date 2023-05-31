d4c8d0868f760b51ad0e74c37a2e7981e35eef9b
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-05-31 14:29:27 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2792 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [n, k] = input[0].split(" ");
+
+function solution(n, k) {
+  const queue = [];
+  const answer = [];
+  for (let i = 0; i < n; i++) queue.push(i + 1);
+
+  let cnt = 1;
+
+  while (queue.length) {
+    const shiftItem = queue.shift();
+    if (cnt % k === 0) {
+      answer.push(shiftItem);
+    } else {
+      queue.push(shiftItem);
+    }
+    cnt++;
+  }
+  console.log(`<${answer.join(", ")}>`);
+}
+
+solution(n, k);
