bb1d5485099d829f2aaf92d437e3828c730e4922
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-08-29 21:41:05 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 7.36 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    let stack = [];
+    let answer = [];
+    
+    [...s].forEach((str) => {
+        if(!stack.includes(str)) {
+            answer.push(-1);
+        }
+        
+        if(stack.includes(str)) {
+            answer.push(stack.length - stack.lastIndexOf(str));
+            
+        }
+        stack.push(str);
+    });
+    return answer;
+}
