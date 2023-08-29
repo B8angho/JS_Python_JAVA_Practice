4d900e6b83f4d94709b24c40e4c7923b90a1f5d6
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-08-29 21:28:22 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,9 +1,3 @@
 function solution(my_string, is_prefix) {
-    const answer = [];
-    const prefix = [...my_string].reduce((a, c, i) => {
-        const sliceWord = my_string.slice(0, i+1);
-        
-        return [...a, sliceWord];
-    },[])
-    return prefix.includes(is_prefix) ? 1 : 0;
+    return my_string.slice(0, is_prefix.length) === is_prefix ? 1 : 0
 }
