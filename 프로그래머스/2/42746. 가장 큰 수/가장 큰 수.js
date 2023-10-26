c7d8484e77971aeac51ecb3a170968aaf50c8347
BYEONGHO JEON <73768243+B8angho@users.noreply.github.com>
2023-10-26 12:48:58 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+  const answer = numbers
+    .map((v) => String(v))
+    .sort((a, b) => b + a - (a + b))
+    .join('');
+
+  return answer[0] === '0' ? '0' : answer;
+}
